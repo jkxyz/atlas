6349fbda2613f25ca1b9c6e7fdd20980cac87f81
Josh Kingsley <josh@joshkingsley.me>
2024-07-06 12:45:41 +0300

    Add just

---
+++ Justfile
@@ -0,0 +1,10 @@
+@_default:
+    just --list
+
+# activate NixOS config for current host
+activate:
+    nix run .#activate
+
+# update primary Flake inputs
+update:
+    nix run .#update
--- flake.nix
+++ flake.nix
@@ -1,29 +1,47 @@
 {
-  description = "Description for the project";
+  description = "Atlas Nix operating system";
 
   inputs = {
-    flake-parts.url = "github:hercules-ci/flake-parts";
+    # # Primary Inputs
+    # Update with `nix run .#update`
+
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+
+    # # Flake Modules
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    nixos-flake.url = "github:srid/nixos-flake";
   };
 
-  outputs = inputs@{ flake-parts, ... }:
+  outputs =
+    inputs@{ flake-parts, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
-      imports = [
-        # To import a flake module
-        # 1. Add foo to inputs
-        # 2. Add foo as a parameter to the outputs function
-        # 3. Add here: foo.flakeModule
+      imports = [ inputs.nixos-flake.flakeModule ];
 
-      ];
-      systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
-      perSystem = { config, self', inputs', pkgs, system, ... }: {
-        # Per-system attributes can be defined here. The self' and inputs'
-        # module parameters provide easy access to attributes of the same
-        # system.
+      systems = [ "x86_64-linux" ];
 
-        # Equivalent to  inputs'.nixpkgs.legacyPackages.hello;
-        packages.default = pkgs.hello;
-      };
+      perSystem =
+        {
+          config,
+          self',
+          inputs',
+          pkgs,
+          system,
+          ...
+        }:
+        {
+          # Flake inputs to be updated periodically
+          # To update, run `nix run .#update`
+          nixos-flake.primary-inputs = [
+            "nixpkgs"
+            "home-manager"
+          ];
+
+          devShells.default = pkgs.mkShell { packages = with pkgs; [ just ]; };
+        };
+
       flake = {
         # The usual flake attributes can be defined here, including system-
         # agnostic ones like nixosModule and system-enumerating ones, although
